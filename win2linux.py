f5ea78d43110f3a6cc868b785ce9fc58fe521c8b
NadieFiind <nadiefiind@gmail.com>
2022-05-27 20:55:14 +0800

    Use DMZ-White for Linux cursors

--- win2linux.py
+++ win2linux.py
@@ -6,33 +6,40 @@ from typing import Dict, List
 
 # Conversion equivalent
 # Keys: Windows cursor names + extra (Keqing Hand, Keqing Grabbing Hand)
-# Value: whiteglass cursor names + some DMZ-White cursor names
+# Value: DMZ-White cursor names
 equiv: Dict[str, List[str]] = {
-	"Alternate Select.cur": ["center_ptr", "right_ptr"],
+	"Alternate Select.cur": ["right_ptr", "draft_large", "draft_small"],
 	"Busy.ani": ["watch"],
 	"Diagonal Resize (Backward).cur": [
-		"bottom_right_corner", "lr_angle", "top_left_corner", "ul_angle"
+		"bd_double_arrow", "bottom_right_corner", "lr_angle", "top_left_corner",
+		"ul_angle"
 	],
 	"Diagonal Resize (Forward).cur": [
-		"bottom_left_corner", "ll_angle", "top_right_corner", "ur_angle"
+		"bottom_left_corner", "fd_double_arrow", "ll_angle", "top_right_corner",
+		"ur_angle"
 	],
-	"Handwriting.cur": ["pencil"],
-	"Help Select.cur": ["question_arrow"],
+	"Handwriting.cur": ["color-picker", "copy", "pencil"],
+	"Help Select.cur": ["question_arrow", "help", "left_ptr_help"],
 	"Horizontal Resize.cur": [
 		"left_side", "left_tee", "right_side", "right_tee", "sb_h_double_arrow",
-		"sb_left_arrow", "sb_right_arrow"
+		"h_double_arrow", "sb_left_arrow", "sb_right_arrow"
 	],
-	"Keqing Grabbing Hand.cur": ["grabbing"],
-	"Keqing Hand.cur": ["hand1", "hand2"],
-	"Link Select.cur": ["sb_down_arrow"],
-	"Move.cur": ["sizing"],
-	"Normal Select.cur": ["left_ptr"],
-	"Precision Select.cur": ["cross"],
+	"Keqing Grabbing Hand.cur": [
+		"grabbing", "fleur", "dnd-none", "dnd-link", "dnd-move", "dnd-copy",
+		"dnd-ask"
+	],
+	"Keqing Hand.cur": [],
+	"Link Select.cur": ["link", "hand", "hand1", "hand2"],
+	"Move.cur": ["move"],
+	"Normal Select.cur": ["left_ptr", "arrow", "top_left_arrow"],
+	"Precision Select.cur": [
+		"cross", "cross_reverse", "diamond_cross", "crosshair", "plus", "tcross"
+	],
 	"Text Select.cur": ["xterm"],
-	"Unavailable.cur": ["X_cursor"],
+	"Unavailable.cur": ["X_cursor", "pirate", "circle", "crossed_circle"],
 	"Vertical Resize.cur": [
-		"base_arrow_down", "base_arrow_up", "bottom_side", "bottom_tee",
-		"double_arrow", "sb_up_arrow", "sb_v_double_arrow", "top_side", "top_tee"
+		"bottom_side", "bottom_tee", "sb_down_arrow", "sb_up_arrow",
+		"sb_v_double_arrow", "double_arrow", "v_double_arrow", "top_side", "top_tee"
 	],
 	"Working in Background.ani": ["left_ptr_watch"]
 }
